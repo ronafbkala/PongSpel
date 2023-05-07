7d712cceec246464dfb55ae9bc5a702dac517e1d
HarisHaris02 <129656502+HarisHaris02@users.noreply.github.com>
2023-05-07 20:32:50 +0200

    Better collision detection

--- PngProj/ball.c
+++ PngProj/ball.c
@@ -3,6 +3,18 @@
 #include <math.h>
 
 #define PI 3.14159265f
+int lastCollidedDown = 0;
+int lastCollidedUp = 0;
+int lastCollidedLeft = 0;
+int lastCollidedRight = 0;
+
+int lastCollidedDownWall = 0;
+int lastCollidedUpWall = 0;
+int lastCollidedLeftWall = 0;
+int lastCollidedRightWall = 0;
+
+
+
 void update_ball_speed(Ball* ball){
     if(ball->speed < 300.0f){
         ball->speed *= 1.1f;
@@ -14,52 +26,176 @@ void initialize_ball(Ball* ball, SDL_Renderer* renderer) {
     ball->y = 300;
     ball->dx = cosf(45 * PI / 180);
     ball->dy = sinf(45 * PI / 180);
-    ball->speed = 100;
+    ball->speed = 200;
     ball->radius = 25;
     SDL_Surface* surface = IMG_Load("ball.png");
     ball->texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_FreeSurface(surface);
 }
 
+SDL_Renderer *renderer = NULL;
+
 
 void update_ball(Ball* ball, Paddle* paddle, Paddle* paddle2, Paddle* paddle3, Paddle* paddle4, float delta_time) {
     ball->x += ball->dx * ball->speed * delta_time;
     ball->y += ball->dy * ball->speed * delta_time;
 
-    // check collision with paddles
-    if (ball->y + ball->radius >= paddle->y && ball->y - ball->radius <= paddle->y + paddle->height &&
-        ball->x + ball->radius >= paddle->x && ball->x - ball->radius <= paddle->x + paddle->width) {
-        ball->dy *= -1;
-        update_ball_speed(ball);
-    }
+    // Create kollision detectors
+    SDL_Rect ballRect = {
+        .x = ball->x,
+        .y = ball->y,
+        .w = ball->radius,
+        .h = ball->radius,
+    };
 
-    if (ball->y + ball->radius >= paddle2->y && ball->y - ball->radius <= paddle2->y + paddle2->height &&
-        ball->x + ball->radius >= paddle2->x && ball->x - ball->radius <= paddle2->x + paddle2->width) {
-        ball->dx *= -1;
+    SDL_Rect playerDownRect = {
+        .x = paddle->x,
+        .y = paddle->y,
+        .w = paddle->width,
+        .h = paddle->height,
+    };
+
+    SDL_Rect playerRightRect = {
+        .x = paddle2->x,
+        .y = paddle2->y,
+        .w = paddle2->width,
+        .h = paddle2->height,
+    };
+
+    SDL_Rect playerUpRect = {
+        .x = paddle3->x,
+        .y = paddle3->y,
+        .w = paddle3->width,
+        .h = paddle3->height,
+    };
+
+    SDL_Rect playerLeftRect = {
+        .x = paddle4->x,
+        .y = paddle4->y,
+        .w = paddle4->width,
+        .h = paddle4->height,
+    };
+// check collision with paddles
+    if(SDL_HasIntersection(&ballRect, &playerDownRect)){
+        if(!lastCollidedDown){
+            printf("Direction change down \n");
+            
+            ball->dy *= -1;
+            lastCollidedDown++;
+            lastCollidedDownWall++;
+        }            
+        lastCollidedLeft = 0;
+        lastCollidedRight = 0;
+        lastCollidedUp = 0;
+        lastCollidedLeftWall = 0;
+        lastCollidedRightWall = 0;
+        lastCollidedUpWall = 0;
         
-        update_ball_speed(ball);
-
     }
 
-    if (ball->y + ball->radius >= paddle3->y && ball->y - ball->radius <= paddle3->y + paddle3->height &&
-        ball->x + ball->radius >= paddle3->x && ball->x - ball->radius <= paddle3->x + paddle3->width) {
-        ball->dy *= -1;
-        update_ball_speed(ball);
-
+    if(SDL_HasIntersection(&ballRect, &playerRightRect)){
+        if(!lastCollidedRight){
+            printf("Direction change right\n");
+            ball->dx *= -1;
+            lastCollidedRight++;
+            lastCollidedRightWall++;
+        }
+        lastCollidedDown = 0;
+        lastCollidedLeft = 0;
+        lastCollidedUp = 0;
+        lastCollidedDownWall = 0;
+        lastCollidedLeftWall = 0;
+        lastCollidedUpWall = 0;
+        
     }
 
-    if (ball->y + ball->radius >= paddle4->y && ball->y - ball->radius <= paddle4->y + paddle4->height &&
-        ball->x + ball->radius >= paddle4->x && ball->x - ball->radius <= paddle4->x + paddle4->width) {
-        ball->dx *= -1;
-        update_ball_speed(ball);
+    if(SDL_HasIntersection(&ballRect, &playerUpRect)){
+        if(!lastCollidedUp){
+            printf("Direction change up\n");
+            ball->dy *= -1;
+            lastCollidedUp++;
+            lastCollidedUpWall++;
+        }
+        lastCollidedDown = 0;
+        lastCollidedLeft = 0;
+        lastCollidedRight = 0;
+        lastCollidedDownWall = 0;
+        lastCollidedLeftWall = 0;
+        lastCollidedRightWall = 0;
+    }
+
+    if(SDL_HasIntersection(&ballRect, &playerLeftRect)){
+        if(!lastCollidedLeft){
+           printf("Direction change left\n");
+           ball->dx *= -1;
+           lastCollidedLeft++;
+           lastCollidedLeftWall++;
+        }
+        lastCollidedDown = 0;
+        lastCollidedRight = 0;
+        lastCollidedUp = 0;
+        lastCollidedDownWall = 0;
+        lastCollidedRightWall = 0;
+        lastCollidedUpWall = 0;
     }
 
     // check collision with walls
-    if (ball->x - ball->radius < 0 || ball->x + ball->radius > 800) {
-        ball->dx *= -1;
+    if (ball->x - ball->radius < 0) {
+        if(!lastCollidedLeftWall){
+            printf("Direction change wall left\n");
+            ball->dx *= -1;
+            lastCollidedLeftWall++;
+            lastCollidedLeft++;
+        }
+        lastCollidedDownWall = 0;
+        lastCollidedRightWall = 0;
+        lastCollidedUpWall = 0;
+        lastCollidedDown = 0;
+        lastCollidedRight = 0;
+        lastCollidedUp = 0;
     }
-    if (ball->y - ball->radius < 0 || ball->y + ball->radius > 600) {
-        ball->dy *= -1;
+
+    if (ball->x + ball->radius > 800) {
+        if(!lastCollidedRightWall){
+            printf("Direction change wall right\n");
+            ball->dx *= -1;
+            lastCollidedRightWall++;
+            lastCollidedRight++;
+        }
+        lastCollidedDownWall = 0;
+        lastCollidedLeftWall = 0;
+        lastCollidedUpWall = 0;
+        lastCollidedDown = 0;
+        lastCollidedLeft = 0;
+        lastCollidedUp = 0;
+    }
+    if (ball->y - ball->radius < 0) {
+        if(!lastCollidedUp){
+            printf("Direction change wall up\n");
+            ball->dy *= -1;
+            lastCollidedUpWall++;
+            lastCollidedUp++;
+        }
+        lastCollidedDownWall = 0;
+        lastCollidedLeftWall = 0;
+        lastCollidedRightWall = 0;
+        lastCollidedDown = 0;
+        lastCollidedLeft = 0;
+        lastCollidedRight = 0;
+    }
+    if (ball->y + ball->radius > 600) {
+        if(!lastCollidedDownWall){
+            printf("Direction change wall down\n");
+            ball->dy *= -1;
+            lastCollidedDownWall++;
+            lastCollidedDown++;
+        }
+        lastCollidedUpWall = 0;
+        lastCollidedLeftWall = 0;
+        lastCollidedRightWall = 0;
+        lastCollidedUp = 0;
+        lastCollidedLeft = 0;
+        lastCollidedRight = 0;
     }
 }
 
