e769bcad31e01fc5487a09b7765b175b28419fa4
HarisHaris02 <129656502+HarisHaris02@users.noreply.github.com>
2023-05-10 20:11:03 +0200

    Tog bort gamla versionen

--- PngProj/application.h
+++
@@ -1,7 +0,0 @@
-#ifndef APPLICATION_H
-#define APPLICATION_H
-
-
-void run_application();
-
-#endif /* APPLICATION_H */
--- PngProj/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <SDL2/SDL.h>
-#include "application.h"
-
-int main(int argc, char* argv[])
-{
-    run_application();
-    return 0;
-}
