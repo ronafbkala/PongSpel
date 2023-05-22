b8aeadf9f8a1416d288d3be6d81b0dde3232fbba
HidraKourie <130184843+HidraKourie@users.noreply.github.com>
2023-05-22 23:27:00 +0200

    Update counter.h

--- PongSpel-main/PongSpel/counter.h
+++ PongSpel-main/PongSpel/counter.h
@@ -9,10 +9,7 @@
 #include "collision.h"
 #include <stdbool.h>
 
-
-
-
-  typedef struct {                      
+typedef struct {                      
     int score;
             
    /* char name[50];
@@ -20,12 +17,11 @@
 }  Player;
 
 
-
-
 void initializeScore(Player *all_players_info);
-
 void drawScore(Player *all_players_info,TTF_Font* font, SDL_Renderer *renderer, SDL_Window *window);
-
 int check_paddle_life(Paddle *paddle, int *live_chance);
+bool checkWinner(Player* all_players_info, int numPlayers, int* winnerIndex);
+void endGame(SDL_Renderer* renderer, TTF_Font* font, SDL_Window* window, int *winnerIndex);
+
 
 #endif
