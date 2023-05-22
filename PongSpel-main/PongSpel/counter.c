ca4dde4b3e1c080e088dfa63c6b01d2d2533b138
HidraKourie <130184843+HidraKourie@users.noreply.github.com>
2023-05-22 23:25:29 +0200

    Update counter.c

--- PongSpel-main/PongSpel/counter.c
+++ PongSpel-main/PongSpel/counter.c
@@ -46,7 +46,7 @@ SDL_Rect scoreRect[] = {
 
 
  // To draw the score on the top of the screen for all players, put it as a string text i case adding some texts later 
-void drawScore(Player* all_players_info, TTF_Font* font, SDL_Renderer* renderer, SDL_Window* window) {
+/*void drawScore(Player* all_players_info, TTF_Font* font, SDL_Renderer* renderer, SDL_Window* window) {
     const int numPlayers = 4;
     char scoreText[numPlayers][SCORE_TEXT_SIZE];
     SDL_Surface* textSurface[numPlayers];
@@ -69,6 +69,71 @@ void drawScore(Player* all_players_info, TTF_Font* font, SDL_Renderer* renderer,
         SDL_FreeSurface(textSurface[i]);
         SDL_DestroyTexture(textTexture[i]);
     }
+}*/
+void drawScore(Player* all_players_info, TTF_Font* font, SDL_Renderer* renderer, SDL_Window* window) {
+    const int numPlayers = 4;
+    char scoreText[numPlayers][SCORE_TEXT_SIZE];
+    SDL_Surface* textSurface[numPlayers];
+    SDL_Texture* textTexture[numPlayers];
+
+    for (int i = 0; i < numPlayers; i++) {
+        if (all_players_info[i].score > 0) {
+            snprintf(scoreText[i], sizeof(scoreText[i]), "%d", all_players_info[i].score);
+        } else {
+            snprintf(scoreText[i], sizeof(scoreText[i]), "Player %d loses", i + 1);
+        }
+
+        textSurface[i] = TTF_RenderText_Solid(font, scoreText[i], (SDL_Color){ 255, 255, 255, 255 });
+        textTexture[i] = SDL_CreateTextureFromSurface(renderer, textSurface[i]);
+
+        SDL_RenderCopy(renderer, textTexture[i], NULL, &scoreRect[i]);
+    }
+
+    for (int i = 0; i < numPlayers; i++) {
+        SDL_FreeSurface(textSurface[i]);
+        SDL_DestroyTexture(textTexture[i]);
+    }
+}
+bool checkWinner(Player* all_players_info, int numPlayers, int* winnerIndex) {
+    int count = 0;
+    
+    for (int i = 0; i < numPlayers; i++) {
+        if (all_players_info[i].score > 0) {
+            count++;
+            *winnerIndex = i;
+        }
+    }
+    
+    if (count == 1) {
+        printf("Player %d is the winner!\n", *winnerIndex + 1);
+        return true;
+    }
+    
+    return false;
+}
+
+
+void endGame(SDL_Renderer* renderer, TTF_Font* font, SDL_Window* window, int *winnerIndex) {
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    
+    char winnerText[50];
+    snprintf(winnerText, sizeof(winnerText), "Player %d is the winner!", *winnerIndex + 1);
+    
+    SDL_Surface* messageSurface = TTF_RenderText_Solid(font, winnerText, (SDL_Color){ 255, 255, 255, 255 });
+    SDL_Texture* messageTexture = SDL_CreateTextureFromSurface(renderer, messageSurface);
+    
+    int messageWidth = messageSurface->w;
+    int messageHeight = messageSurface->h;
+    SDL_Rect messageRect = { (800 - messageWidth) / 2, (600 - messageHeight) / 2, messageWidth, messageHeight };
+    
+    SDL_RenderCopy(renderer, messageTexture, NULL, &messageRect);
+    
+    SDL_FreeSurface(messageSurface);
+    SDL_DestroyTexture(messageTexture);
+    
+    SDL_RenderPresent(renderer);
+    
 }
 
 
