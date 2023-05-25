a605348216f43fe30641ff5647a49c5f753f44a6
Rona Faraj Butrus Kala <rfbkala@kth.se>
2023-05-26 01:01:09 +0200

    fortsätta med bildarna

--- PongSpel-main/PongSpel/application.c
+++ PongSpel-main/PongSpel/application.c
@@ -310,6 +310,7 @@ void run_application()
     SDL_Texture *join_texture = load_texture(renderer, "src/join.png");
     SDL_Texture *info_texture = load_texture(renderer, "src/info.png");
     SDL_Texture *instruction_texture = load_texture(renderer, "src/instruction1.png");
+    SDL_Texture *background_game_texture = load_texture(renderer, "src/background.jpg");
     SDL_Texture *win_texture = load_texture(renderer, "src/win.jpg");
 
     // Set button position and size
